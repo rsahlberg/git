8779a55498c8119d97d497906829db529df28562
Ronnie Sahlberg <sahlberg@google.com>
2014-05-01 10:40:10 -0700

    refs.c: move the check for valid refname to lock_ref_sha1_basic
    Move the check for check_refname_format from lock_any_ref_for_update
    to lock_ref_sha1_basic. At some later stage we will get rid of
    lock_any_ref_for_update completely.
    
    This leaves lock_any_ref_for_updates as a no-op wrapper which could be removed.
    But this wrapper is also called from an external caller and we will soon
    make changes to the signature to lock_ref_sha1_basic that we do not want to
    expose to that caller.
    
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>

--- refs.c
+++ refs.c
@@ -2042,6 +2042,9 @@ static struct ref_lock *lock_ref_sha1_basic(const char *refname,
 	int missing = 0;
 	int attempts_remaining = 3;
 
+	if (check_refname_format(refname, REFNAME_ALLOW_ONELEVEL))
+		return NULL;
+
 	lock = xcalloc(1, sizeof(struct ref_lock));
 	lock->lock_fd = -1;
 
@@ -2133,8 +2136,6 @@ struct ref_lock *lock_any_ref_for_update(const char *refname,
 					 const unsigned char *old_sha1,
 					 int flags, int *type_p)
 {
-	if (check_refname_format(refname, REFNAME_ALLOW_ONELEVEL))
-		return NULL;
 	return lock_ref_sha1_basic(refname, old_sha1, flags, type_p);
 }
 
