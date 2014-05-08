c38ca56969ffd3a9b24089d46323f9373a7634ec
Ronnie Sahlberg <sahlberg@google.com>
2014-05-08 12:34:53 -0700

    refs.c: rename the transaction functions
    Rename the transaction functions. Remove the leading ref_ from the
    names and append _sha1 to the names for functions that create/delete/
    update sha1 refs.
    
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>

--- builtin/replace.c
+++ builtin/replace.c
@@ -158,14 +158,14 @@ static int replace_object(const char *object_ref, const char *replace_ref,
 	else if (!force)
 		die("replace ref '%s' already exists", ref);
 
-	transaction = ref_transaction_begin(&err);
+	transaction = transaction_begin(&err);
 	if (!transaction ||
-	    ref_transaction_update(transaction, ref, repl, prev,
-				   0, !is_null_sha1(prev), NULL, &err) ||
-	    ref_transaction_commit(transaction, &err))
+	    transaction_update_sha1(transaction, ref, repl, prev,
+				    0, !is_null_sha1(prev), NULL, &err) ||
+	    transaction_commit(transaction, &err))
 		die("%s", err.buf);
 
-	ref_transaction_free(transaction);
+	transaction_free(transaction);
 	return 0;
 }
 
